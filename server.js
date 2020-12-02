fe41df37b95709fa7e18078fed76dd3616da7379
Janya Joshi <60742558+janyajoshi@users.noreply.github.com>
2020-12-02 17:33:13 +0530

    updated login and Introduced Homepage

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express()
 const port = 9000
 app.set("view engine","html")
 app.use(express.static('public'))
+var http = require('http').createServer(app)
+var io = require('socket.io')(http);
 
 
 
@@ -11,14 +13,27 @@ app.get(`/`,(req,res)=>{
   res.send('index')
 })
 
-app.post(`/cred`,(req,res)=>{         
+/*app.all(`/cred`,(req,res)=>{         
   req.on('data',(a)=>{
     var cred = JSON.parse(a.toString())
     console.log(cred.username);
     console.log(cred.password);
   })
+})*/
+
+io.on('connection', (socket) => {
+  //console.log('user connected');
+  socket.on('cred',(a)=>{
+    var cred = JSON.parse(a.toString())
+    console.log(cred.username);
+    console.log(cred.password);
+    //authenticate here
+    socket.emit('status',1) //respond with 1 if login allowed, 
+
+  })
+
 })
 
-app.listen(port, ()=>{
+http.listen(port, ()=>{
     console.log("Server started")
 })
